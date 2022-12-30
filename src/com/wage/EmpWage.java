063ff7544b7db473e447bbdbe833158948abfe7d
Rushikesh <rushikeshdevale2658@gmail.com>
2022-12-30 17:22:32 +0530

    Using Case

--- src/com/wage/EmpWage.java
+++ src/com/wage/EmpWage.java
@@ -2,29 +2,29 @@ package com.wage;
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation program !! ");
-        int is_fulltime_present = 1;
-        int is_parttime_present = 2;
+        int is_fullTime_present = 1;
+        int is_partTime_present = 2;
         int daily_wage;
         int wage_per_hr = 20;
         int working_hr;
         int emp_check = (int)(Math.floor(Math.random() * 10)) % 3;
-        if( emp_check == is_fulltime_present)
-        {
-            System.out.println("Employee is full time present");
-            working_hr = 8;
+        switch (emp_check) {
+            case 1:
+                System.out.println("Employee is full time present");
+                working_hr = 8;
+                break;
+            case 2:
+                System.out.println("Employee is part time present");
+                working_hr = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
+                working_hr = 0;
         }
-        else if( emp_check == is_parttime_present)
-        {
-            System.out.println("Employee is part time present");
-            working_hr = 4;
-        }
-        else
-        {
-            System.out.println("Employee is absent");
-            working_hr = 0;
-        }
-
         daily_wage = working_hr * wage_per_hr;
         System.out.println("Employee daily wage is " + daily_wage);
     }
-}
+
+
+    }
+
