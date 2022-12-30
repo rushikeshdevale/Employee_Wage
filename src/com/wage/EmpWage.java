8f8133968439f0f3e49713f9c5acb4f8977acb84
Rushikesh <rushikeshdevale2658@gmail.com>
2022-12-30 16:51:36 +0530

    Welcome

---
+++ src/com/wage/EmpWage.java
@@ -0,0 +1,15 @@
+package com.wage;
+public class EmpWage {
+    public static void main(String[] args) {
+
+            System.out.println(" Welcome to Employee Wage Computation program !! ");
+            int is_present = 1;
+            int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
+            if( emp_check == is_present)
+            {
+                System.out.println("Employee is present");
+            }
+            else
+                System.out.println("Employee is absent");
+        }
+}
